119981824c33d5f47a92eb3114cd4b888c605e35
Pri-Alves <priscillarafaela19@gmail.com>
2021-06-22 17:07:20 -0300

    conexão com banco de dados

--- clientes/src/main/java/io/github/PriAlves/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/io/github/PriAlves/clientes/model/entity/Cliente.java
@@ -1,13 +1,15 @@
 package io.github.PriAlves.clientes.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +24,8 @@ public class Cliente {
     @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
 
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
 }
