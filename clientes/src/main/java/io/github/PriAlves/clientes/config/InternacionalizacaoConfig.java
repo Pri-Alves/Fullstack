cee5b6599eeaf6256679db08b0da62d3f3644c56
Pri-Alves <priscillarafaela19@gmail.com>
2021-07-06 17:43:31 -0300

    Configurando a Internacionalizacao

---
+++ clientes/src/main/java/io/github/PriAlves/clientes/config/InternacionalizacaoConfig.java
@@ -0,0 +1,29 @@
+package io.github.PriAlves.clientes.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
+@Configuration
+public class InternacionalizacaoConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("messages"); //o ResourceBundle ja entende que messages esta em properties
+        messageSource.setDefaultEncoding("ISO-8859-1"); //para reconhecer os caracteres brasileiros
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){ //faz a interpolaçao/integração entre as mensagens e a nossa validação da especifição das validações do java (nome,cpf)
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
