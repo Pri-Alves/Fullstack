c96a4a0ebabd2fefb95495132deb6468050adf95
Pri-Alves <priscillarafaela19@gmail.com>
2021-07-06 16:42:55 -0300

    Obtendo dados de um Cliente - Get

--- clientes/src/main/java/io/github/PriAlves/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/io/github/PriAlves/clientes/model/entity/Cliente.java
@@ -2,6 +2,8 @@ package io.github.PriAlves.clientes.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.*;
 import java.time.LocalDate;
--- clientes/src/main/java/io/github/PriAlves/clientes/rest/ClienteController.java
+++ clientes/src/main/java/io/github/PriAlves/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.PriAlves.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController //controller + responseBody
 @RequestMapping ("/api/clientes") //mapear a URL base que será tratada dentro do controller
@@ -22,5 +23,12 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED) //retornar o status created quando salvar um novo cliente no servidor
     public Cliente salvar ( @RequestBody Cliente cliente){ //@RequestBody para dizer que o novo cliente será passado por json no corpo da requisição
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){ //@PathVariable("id") informar o ("id") é opcional porque o nome da url é igual ao da variavel
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)); //ele busca por id e se estiver ok returna 200, caso nao encontre retorna um 404 not found
     }
 }
